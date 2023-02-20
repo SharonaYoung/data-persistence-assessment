2a79775266be3a062e0a3fb4c208b8bf4c679a0d
Sharona Young <msrona806@gmail.com>
2023-02-20 13:29:07 -0500

    added template for displaying city and weather info

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const cityEl = document.querySelector('#city');
 const search = document.querySelector('form');
 // API: api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=f2f399cdf34a0ffbfc30ea40696a33b5&units=imperial
 
+// variable to hold city to search
 let city 
 
 search.addEventListener('submit', async function(event){
@@ -10,8 +11,30 @@ search.addEventListener('submit', async function(event){
   console.log(city);
   cityEl.value = '';
 
-// fetch data from weather map api using async function
-const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f2f399cdf34a0ffbfc30ea40696a33b5&units=imperial`);
-const data = await response.json();
-console.log(data.main.temp);
+  // fetch data from weather map api using async function
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f2f399cdf34a0ffbfc30ea40696a33b5&units=imperial`);
+  const data = await response.json();
+  console.log(data);
+  console.log(data.name);
+  console.log(data.main.temp);
+  showWeather(data);
+  console.log(data.weather);
+  console.log(JSON.stringify(data));
 });
+
+// display weather info to the DOM
+const weatherEL = document.querySelector('#weather');
+
+  function showWeather(weatherData) {
+    const displayWeather = `
+      <h2>${weatherData.name}</h2>
+      <h3>Temperature: </h3>
+      <ul>
+        <li>Current Temperature: ${weatherData.main.temp}</li>
+        <li>Low: ${weatherData.main.temp_min}</li>
+        <li>High: ${weatherData.main.temp_max}</li>
+      </ul>
+      <h3>Weather Conditions: ${weatherData.weather.value}</h3>
+  `
+  weatherEL.innerHTML = displayWeather;
+}
