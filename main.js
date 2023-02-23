875318c7ba32b58c9d8f72a64974eeeffac6964c
Sharona Young <msrona806@gmail.com>
2023-02-22 19:59:09 -0500

    cleaned up empty lines

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const cityEl = document.querySelector('#city');
 const search = document.querySelector('form');
-// API: api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=f2f399cdf34a0ffbfc30ea40696a33b5&units=imperial
 
 // variable to hold city to search
 let city 
@@ -8,7 +7,6 @@ let city
 search.addEventListener('submit', async function(event){
   event.preventDefault();
   city = cityEl.value;
-  console.log(city);
   cityEl.value = '';
 
   // fetch data from weather map api using async function
@@ -22,15 +20,15 @@ search.addEventListener('submit', async function(event){
 // display weather info to the DOM
 const weatherEL = document.querySelector('#weather');
 
-  function showWeather(weatherData) {
-    const displayWeather = `
-      <h2>${weatherData.sys.country} | ${weatherData.name}</h2>
-      <h3>Current Temperature: ${Math.round(weatherData.main.temp)}&deg;</h3>
-      <ul>
-        <li>Low: ${Math.round(weatherData.main.temp_min)}</li>
-        <li>High: ${Math.round(weatherData.main.temp_max)}</li>
-        <li>Weather Conditions: ${weatherData.weather[0].main}</li>
-      </ul>
+function showWeather(weatherData) {
+  const displayWeather = `
+    <h2>${weatherData.sys.country} | ${weatherData.name}</h2>
+    <h3>Current Temperature: ${Math.round(weatherData.main.temp)}&deg;</h3>
+    <ul>
+      <li>Low: ${Math.round(weatherData.main.temp_min)}</li>
+      <li>High: ${Math.round(weatherData.main.temp_max)}</li>
+      <li>Weather Conditions: ${weatherData.weather[0].main}</li>
+    </ul>
   `
   weatherEL.innerHTML = displayWeather;
 }
@@ -40,4 +38,3 @@ const lastCity = localStorage.getItem('last_city');
 if (lastCity) {
   showWeather(JSON.parse(lastCity))
 }
-// http://api.openweathermap.org/geo/1.0/reverse?lat=51.5098&lon=-0.1180&limit=5&appid=f2f399cdf34a0ffbfc30ea40696a33b5
