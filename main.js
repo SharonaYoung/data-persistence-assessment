2221d52c26452e38a33e6a3a36ef07a7ef03fdc0
Sharona Young <msrona806@gmail.com>
2023-02-22 19:55:44 -0500

    added local storage rule

--- main.js
+++ main.js
@@ -14,12 +14,9 @@ search.addEventListener('submit', async function(event){
   // fetch data from weather map api using async function
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f2f399cdf34a0ffbfc30ea40696a33b5&units=imperial`);
   const data = await response.json();
-  console.log(data);
-  console.log(data.name);
-  console.log(data.main.temp);
+  // set local storage key
+  localStorage.setItem('last_city', JSON.stringify(data));
   showWeather(data);
-  console.log(data.weather[0].main);
-  console.log(JSON.stringify(data.weather));
 });
 
 // display weather info to the DOM
@@ -27,13 +24,20 @@ const weatherEL = document.querySelector('#weather');
 
   function showWeather(weatherData) {
     const displayWeather = `
-      <h2>${weatherData.name}</h2>
-      <h3>Current Temperature: ${Math.round(weatherData.main.temp)} </h3>
+      <h2>${weatherData.sys.country} | ${weatherData.name}</h2>
+      <h3>Current Temperature: ${Math.round(weatherData.main.temp)}&deg;</h3>
       <ul>
         <li>Low: ${Math.round(weatherData.main.temp_min)}</li>
         <li>High: ${Math.round(weatherData.main.temp_max)}</li>
+        <li>Weather Conditions: ${weatherData.weather[0].main}</li>
       </ul>
-      <h3>Weather Conditions: ${weatherData.weather[0].description}</h3>
   `
   weatherEL.innerHTML = displayWeather;
 }
+
+// Show most recent city searched
+const lastCity = localStorage.getItem('last_city');
+if (lastCity) {
+  showWeather(JSON.parse(lastCity))
+}
+// http://api.openweathermap.org/geo/1.0/reverse?lat=51.5098&lon=-0.1180&limit=5&appid=f2f399cdf34a0ffbfc30ea40696a33b5
